f5a29ceddb2dfee811ba22940ef6f192db9918eb
Maxime50 <maxime.romero50@gmail.com>
2021-02-21 03:21:47 -0500

    update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
           onClick={() => onRouteChange('signout')}
           className="f3 link dim black underline pa3 pointer"
         >
-          Sign Out
+          Déconnexion
         </p>
       </nav>
     );
