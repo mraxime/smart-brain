59749205cf737103ceccca5fb7cd8ee1e40534b0
Maxime50 <maxime.romero50@gmail.com>
2021-02-21 03:22:46 -0500

    update

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className='white f3'>
-        {`${name} , your current entry count is...`}
+      <div className="white f3">
+        {`${name} , votre nombre actuel d'entrées est de...`}
       </div>
-      <div className='white f1'>{entries}</div>
+      <div className="white f1">{entries}</div>
     </div>
   );
 };
