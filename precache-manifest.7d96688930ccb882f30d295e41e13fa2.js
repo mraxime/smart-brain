08d81b0b198f4e8b9008e7200184d0ca438648bc
Maxime50 <maxime.romero50@gmail.com>
2021-02-21 03:20:05 -0500

    Updates

---
+++ precache-manifest.7d96688930ccb882f30d295e41e13fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c8bf19d07add3dfb1c2",
+    "url": "/smart-brain/static/css/main.3c7ed818.chunk.css"
+  },
+  {
+    "revision": "8c8bf19d07add3dfb1c2",
+    "url": "/smart-brain/static/js/main.4c0db388.chunk.js"
+  },
+  {
+    "revision": "b9b46f64ddcbca33fcc3",
+    "url": "/smart-brain/static/js/runtime~main.e63bf16e.js"
+  },
+  {
+    "revision": "381c6e781a7b957e628c",
+    "url": "/smart-brain/static/css/2.9935fd40.chunk.css"
+  },
+  {
+    "revision": "381c6e781a7b957e628c",
+    "url": "/smart-brain/static/js/2.e6e37443.chunk.js"
+  },
+  {
+    "revision": "04d9204fe392ffa78056c83c9e5e7857",
+    "url": "/smart-brain/index.html"
+  }
+];
